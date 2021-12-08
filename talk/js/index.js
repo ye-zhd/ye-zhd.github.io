b1f3c56757488e56a3852df51b9e94cdedc369b5
ye-zhd <2437714134@qq.com>
2021-12-08 22:43:01 +0800

    Site updated: 2021-12-08 22:42:59

---
+++ talk/js/index.js
@@ -0,0 +1,14 @@
+$('#login-button').click(function (event) {
+	var userName=document.getElementById("userName").value;
+    var pwd=document.getElementById("pwd").value;
+		//修改密码请改此处
+    if(userName=="黄美" &&  pwd=="1112"){
+			event.preventDefault();
+			$('form').fadeOut(500);
+			$('.wrapper').addClass('form-success');
+			setTimeout(function(){location.href="BirthdayCake.html";},2000);
+		}
+	else{
+		alert("Wrong Password");
+	}
+});
